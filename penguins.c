5dfedf8d0907066103f6cdc65db9b563e9bf3152
Agrabski <maslox303@gmail.com>
2016-11-06 15:23:39 +0100

    adding check_valid_move function

--- penguins.c
+++ penguins.c
@@ -16,6 +16,34 @@ movement(){
 	
 	}
 write_file();
+int check_valid_move(int x1, int y1, int x2, int y2)
+{
+	int pathClear=1;
+	if((x2-x1)==0&&(y2-y1)%2==0) /* checking fields above or below current field*/
+	{
+		for(int y=y1;y==y2;y+=sign(y2-y1)*2)
+		{
+			if(y<0||y>mapSize||check_penguin(x1,y)!=0||check_how_many_fishes(x1,y)==0)
+			{
+				pathClear=0
+				break;
+			}
+		}
+	}
+	else
+	{
+		for(int x=x1, y=y1;x==x2;x+=sign(x2-x1),y==sign(y2-y1))
+		{
+			if(y<0||x<0||x>mapSize||y>mapSize||check_penguin(x,y)!=0||check_how_many_fishes(x,y)==0)
+			{
+				pathClear=0
+				break;
+			}
+		}
+	}
+	return pathClear;
+	
+}
 
 
 int main(){
