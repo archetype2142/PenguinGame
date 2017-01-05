be9d8fb83a7e3904db54a77dcb86e24635ffa589
Agrabski <maslox303@gmail.com>
2017-01-05 22:33:33 +0100

    fixing the ai and introducing agresion

--- penguin-9000.c
+++ penguin-9000.c
@@ -3,13 +3,15 @@
 #include "Map.h"
 #include "GameLogic.h"
 #include "penguin-9000.h"
+#include <math.h>
 
 struct Vector movePenguin(int playerID, void *mapP, int sizeX, int sizeY, struct Player players[],int numberOfPlayers)
 {
 	struct Vector best={-1,-1,-1,-1};
 	void *mapTMPP = malloc(sizeof(struct Floe)*sizeX*sizeY);
     struct Player *playerstmp=copyplayers(players,numberOfPlayers);
-	int i, direction = 0, distanse = 0, bestvalue = 0, playerIndex=giveIndex(playerID,players,numberOfPlayers), newEvaluate;
+	int i, direction = 0, distanse = 0, playerIndex=giveIndex(playerID,players,numberOfPlayers);
+	float newEvaluate, bestvalue=0;
 	for (i = 0; i < players[playerIndex].numberOfPenguins; i++)//evaluating evry possible move for all penguins of playerID//
 	{
 		for ( direction = 0; direction < 6; direction++)
@@ -51,7 +53,7 @@ struct Point placePenguin(void *mapP, int sizeX, int sizeY, int playerID, struct
 	struct Player *playerstmp;
 	struct Point result={-1,-1};
 	int x, y;
-	int best=0;
+	float best=0;
 	for (y = 0; y < sizeY; y++)
 	{
 		for (x = 0; x < sizeX; x++)
@@ -96,9 +98,10 @@ struct Player *copyplayers(struct Player players[], int playerCount)
     return copy;
 }
 
-int evaluate(void *mapP, int sizeX, int sizeY, int playerID, struct Player players[], int numberOfPlayers)// needs reworking (might be fixed already XD)
+float evaluate(void *mapP, int sizeX, int sizeY, int playerID, struct Player players[], int numberOfPlayers)// needs reworking (might be fixed already XD)
 {
-	int direction, sum=0, i,k;
+	int direction, i,k;
+	float sum=0;
 	for (i = 0; i < numberOfPlayers; i++)
 	{
 		if (players[i].playerID == playerID)
@@ -109,7 +112,7 @@ int evaluate(void *mapP, int sizeX, int sizeY, int playerID, struct Player playe
                 {
                     for (direction = 0; direction < 6; direction++)
                     {
-                        sum += evaluateBranch(mapP, sizeX, sizeY, players[i].penguins[k].x, players[i].penguins[k].y, direction);
+                        sum += (check_how_many_fishes(players[i].penguins[k].x,players[i].penguins[k].y,mapP,sizeX,sizeY))* evaluateBranch(mapP, sizeX, sizeY, players[i].penguins[k].x, players[i].penguins[k].y, direction)/ giveAgression(players,numberOfPlayers,mapP,sizeX,sizeY,playerID);
                     }
                 }
 			}
@@ -124,7 +127,7 @@ int evaluate(void *mapP, int sizeX, int sizeY, int playerID, struct Player playe
                     {
                         for (direction = 0; direction < 6; direction++)
                         {
-                            sum -= evaluateBranch(mapP, sizeX, sizeY, players[i].penguins[k].x, players[i].penguins[k].y, direction);
+                            sum -= (check_how_many_fishes(players[i].penguins[k].x,players[i].penguins[k].y,mapP,sizeX,sizeY))* giveAgression(players,numberOfPlayers,mapP,sizeX,sizeY,playerID) * evaluateBranch(mapP, sizeX, sizeY, players[i].penguins[k].x, players[i].penguins[k].y, direction);
                         }
                     }
                 }
@@ -134,13 +137,14 @@ int evaluate(void *mapP, int sizeX, int sizeY, int playerID, struct Player playe
 	return sum;
 }
 
-int evaluateBranch(void *mapP, int sizeX, int sizeY, int x, int y, int direction)
+float evaluateBranch(void *mapP, int sizeX, int sizeY, int x, int y, int direction)
 {
 	struct Floe(*map)[sizeX][sizeY] = (struct Floe(*)[sizeX][sizeY]) mapP;
-	int i, sum=0;
+	int i;
+	float sum=0;
 	for(i=1; x+i*vectors[direction].x<sizeX && y+i*vectors[direction].y<sizeY && x+i*vectors[direction].x>=0 && y+i*vectors[direction].y>=0 && (*map)[x + i*vectors[direction].x][y + i*vectors[direction].y].numbOfFish!=0 && (*map)[x + i*vectors[direction].x][y + i*vectors[direction].y].whosPenguin==0; i++)
     {
-        sum+=(*map)[x + i*vectors[direction].x][y + i*vectors[direction].y].numbOfFish*(*map)[x + i*vectors[direction].x][y + i*vectors[direction].y].numbOfFish;
+        sum+=sqrt((*map)[x + i*vectors[direction].x][y + i*vectors[direction].y].numbOfFish);
     }
     sum += (*map)[x][y].numbOfFish;
 	return sum;
@@ -161,4 +165,28 @@ void findTarget(struct Vector * Vector, int x, int y, int distance, int directio
 {
 	(*Vector).xTarget = x+vectors[direction].x*distance;
 	(*Vector).yTarget = y+vectors[direction].y*distance;
+}
+
+float giveAgression(struct Player players[], int PlayerCount, struct Floe *mapp, int sizeX, int sizeY, int playerID)
+{
+    float aggresion;
+    int numOfFloes=0, x,y, enemyScore=0;
+   	struct Floe(*map)[sizeX][sizeY] = (struct Floe(*)[sizeX][sizeY]) mapp;
+   	for(y=0;y<sizeY;y++)
+    {
+        for(x=0;x<sizeX;x++)
+            if((*map)[x][y].numbOfFish>0)
+        {
+            numOfFloes++;
+        }
+    }
+    for(x=0;x<PlayerCount;x++)
+    {
+        if(players[x].playerID!=playerID)
+        {
+            enemyScore+=players[x].score;
+        }
+    }
+    aggresion= ((float) players[giveIndex(playerID,players,PlayerCount)].score/ (float) enemyScore)/numOfFloes;
+    return aggresion;
 }
